7f8495ffd998e879d0e2ed6076c39bb80e4f2f4b
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2023-04-06 01:49:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const body = require("body-parser");
 const helmet = require('helmet');
-
+const path = require("path");
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
+
+
+
+// set the views directory
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(body.urlencoded({ extended: true }));
 
